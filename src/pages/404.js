6ba9f5d4a12ca3bebb9f94928258730c52545873
sambrosia <samuelcwoodruff@gmail.com>
2017-10-09 16:08:12 -0600

    Initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import preact from 'preact'
+
+const NotFoundPage = () => (
+  <div>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
+)
+
+export default NotFoundPage
