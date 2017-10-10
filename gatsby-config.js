81a4b0191cfc15866e3e28b680aa86405d1e0e17
sambrosia <samuelcwoodruff@gmail.com>
2017-10-09 19:11:19 -0600

    Add normalize.css and typography.js
    And remove existing styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`, 'gatsby-plugin-preact']
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-preact',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js'
+      }
+    }
+  ]
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,34 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem'
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+import 'normalize.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -41,17 +14,7 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' }
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
 )
 
